fd24108c05287f5449b25f1bfeda76a8f5d7e03e
Malaiarasu G <govindarajmalaiarasu@gmail.com>
2025-09-20 14:45:35 +0000

    it is not connecto - Connect

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -11,7 +11,7 @@ export function Logo({ className, ...props }: React.HTMLAttributes<HTMLDivElemen
         height={32}
         className="rounded-md"
       />
-      <span className="text-xl leading-tight">onnecto</span>
+      <span className="text-xl leading-tight">onnect</span>
     </div>
   )
 }
